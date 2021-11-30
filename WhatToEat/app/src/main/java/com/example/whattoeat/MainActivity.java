c8d0a84f3122d6af3251bd3eb5958dc19ddf13e0
PeelSummer <m1020117@gmail.com>
2021-11-30 14:36:08 +0800

    Program upload

---
+++ WhatToEat/app/src/main/java/com/example/whattoeat/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.whattoeat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginButton(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+}
