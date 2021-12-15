c2484e87ad3c880de8384c95ba4cc3c9d3a7fefe
PeelSummer <m1020117@gmail.com>
2021-12-15 10:24:13 +0800

    更新MainActivity程式碼及些許樣式

--- WhatToEat/app/src/main/java/com/example/whattoeat/activity/MainActivity.java
+++ WhatToEat/app/src/main/java/com/example/whattoeat/activity/MainActivity.java
@@ -3,61 +3,100 @@ package com.example.whattoeat.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
-
 import com.example.whattoeat.dialog.FindPassDialogFragment;
 import com.example.whattoeat.R;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt_account;
+    private EditText edt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_account = findViewById(R.id.edt_account);
+        edt_password = findViewById(R.id.edt_password);
     }
 
     public void eventListener(View view){
         switch (view.getId()){
             case R.id.btn_login:    //會員登入
-                //拿取資料
-                gotoMenu();
+                //比對帳戶是否存在
+                System.out.println("Dialog : account = " + edt_account.getText().toString());
+                System.out.println("Dialog : password = " + edt_password.getText().toString());
+                checkAccount();
                 break;
             case R.id.btn_visitor:  //訪客進入
-                gotoMenu();
+                gotoNextActivity(MenuActivity.class);
                 break;
             case R.id.btn_register: //註冊頁面
-                gotoRegister();
+                gotoNextActivity(RegisterActivity.class);
                 break;
             case R.id.btn_findpass: //忘記密碼
                 FindPassDialog();
                 break;
             case R.id.imgBtn_GoogleIn: //Google登入
-                gotoMenu();
-                break;
-            case R.id.imgBtn_LineIn: //Google登入
-                gotoMenu();
-                break;
-            case R.id.imgBtn_FacebookIn: //Google登入
-                gotoMenu();
+            case R.id.imgBtn_LineIn: //Line登入
+            case R.id.imgBtn_FacebookIn: //Facebook登入
+                externalLogin(view);
                 break;
             default:
                 break;
         }
     }
 
-    //前往菜單
-    public void gotoMenu(){
-        Intent intentMenu = new Intent(this, MenuActivity.class);
-        startActivity(intentMenu);
+    //檢查登入帳號密碼
+    public void checkAccount(){
+        if( edt_account.getText().toString().equals("") || edt_password.getText().toString().equals("")){
+            Toast.makeText(this,"請確實填寫帳號密碼",Toast.LENGTH_LONG).show();
+        }else{
+            if(true){
+                gotoNextActivity(MenuActivity.class);
+            }else{
+                Toast.makeText(this,"帳號或密碼不正確，請重新輸入",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
-    //前往註冊
-    public void gotoRegister(){
-        Intent intentRegister = new Intent(this, RegisterActivity.class);
-        startActivity(intentRegister);
+    //外部登入
+    public void externalLogin(View view){
+        boolean loginSuccess = false;
+        switch (view.getId()){
+            case R.id.imgBtn_GoogleIn: //Google登入
+                if(true){
+                    loginSuccess = true;
+                }
+                break;
+            case R.id.imgBtn_LineIn: //Line登入
+                if(true){
+                    loginSuccess = true;
+                }
+                break;
+            case R.id.imgBtn_FacebookIn: //Facebook登入
+                if(true){
+                    loginSuccess = true;
+                }
+                break;
+        }
+
+        if(loginSuccess){
+            gotoNextActivity(MenuActivity.class);
+        }else{
+            Toast.makeText(this,"登入失敗",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    //前往下一個畫面
+    public void gotoNextActivity(Class nextActiviy){
+        Intent intentNext = new Intent(this, nextActiviy);
+        startActivity(intentNext);
     }
 
     //忘記密碼彈跳視窗
