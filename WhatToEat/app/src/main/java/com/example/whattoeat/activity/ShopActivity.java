58d23bd6cdb379e1b9c00724b415156e31335112
PeelSummer <m1020117@gmail.com>
2021-12-25 16:31:35 +0800

    連接Google地圖

--- WhatToEat/app/src/main/java/com/example/whattoeat/activity/ShopActivity.java
+++ WhatToEat/app/src/main/java/com/example/whattoeat/activity/ShopActivity.java
@@ -1,6 +1,10 @@
 package com.example.whattoeat.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +16,16 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        ImageButton imgBtn_navigate = (ImageButton) findViewById(R.id.imgBtn_navigate);
+        imgBtn_navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri location = Uri.parse("geo:0,0?q=高雄市楠梓區楠梓路188號");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 }
