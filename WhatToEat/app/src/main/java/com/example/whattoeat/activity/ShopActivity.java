e668d41df5bb51ccdb945c05a0e215e05615312d
PeelSummer <m1020117@gmail.com>
2021-12-25 21:08:43 +0800

    調整按下按鈕後的方法 並串接到EvalutionActivity

--- WhatToEat/app/src/main/java/com/example/whattoeat/activity/ShopActivity.java
+++ WhatToEat/app/src/main/java/com/example/whattoeat/activity/ShopActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import com.example.whattoeat.R;
 
@@ -17,15 +17,22 @@ public class ShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
-        ImageButton imgBtn_navigate = (ImageButton) findViewById(R.id.imgBtn_navigate);
-        imgBtn_navigate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri location = Uri.parse("geo:0,0?q=炸魂雞排-高雄楠梓店");
+    }
+
+    public void eventListener(View view){
+        switch (view.getId()){
+            case R.id.imgBtn_navigate: //導航
+                Uri location = Uri.parse("geo:0,0?q=炸魂雞排 高雄市楠梓區楠梓路188號");
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
-            }
-        });
+                break;
+            case R.id.tv_evaluation_more: //更多評價
+                Intent intentNext = new Intent(this, EvaluationActivity.class);
+                startActivity(intentNext);
+                break;
+            default:
+                break;
+        }
     }
 }
